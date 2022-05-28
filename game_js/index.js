c9ae59dde6bd5015713efc7da14822f68a150574
Jetro Oropeza <jetrojackof@hotmail.com>
2022-05-28 16:07:36 -0500

    finish round function

--- game_js/index.js
+++ game_js/index.js
@@ -1,12 +1,26 @@
 function computerPlay(){
-    arr = ["Rock", "Paper", "Scissors"];
+    arr = ["rock", "paper", "scissors"];
     return arr[Math.floor(Math.random()*arr.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
+    console.log(computerSelection)
+    if (playerSelection  === computerSelection) {
+        return "Play again!"
+    } else if ((playerSelection === 'rock') && (computerSelection ==='paper')){
+        return "You Lose! Paper beats Rock"
+    } else if ((playerSelection === 'paper') && (computerSelection ==='scissors')){
+        return "You Lose! Scissors beats Paper"
+    } else if ((playerSelection === 'scissors') && (computerSelection ==='rock')){
+        return "You Lose! Rock beats Scissors"
+    } else ;{
+        return "You Win"
+    }
+
   }
   
   const playerSelection = "rock";
   const computerSelection = computerPlay();
   console.log(playRound(playerSelection, computerSelection))
+
