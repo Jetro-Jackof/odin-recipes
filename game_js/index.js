83d88505bbd5b963fdb66a3eb51b639ee95fc3f3
Jetro Oropeza <jetrojackof@hotmail.com>
2022-05-28 19:56:58 -0500

    game code fix playRound

--- game_js/index.js
+++ game_js/index.js
@@ -3,9 +3,15 @@ function computerPlay(){
     return arr[Math.floor(Math.random()*arr.length)]
 }
 
+function playerPlay(){
+    selection= prompt("Rock Paper or Scissors?")
+    return selection.toLowerCase()
+}
+const computerSelection = computerPlay();
+const playerSelection = playerPlay()
+
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-    console.log(computerSelection)
+
     if (playerSelection  === computerSelection) {
         return "Play again!"
     } else if ((playerSelection === 'rock') && (computerSelection ==='paper')){
@@ -19,8 +25,31 @@ function playRound(playerSelection, computerSelection) {
     }
 
   }
-  
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection))
+    //*Play Round test *//
+  //console.log(playRound(playerSelection, computerSelection))
+
+
+function game(playRound){
+    player = 0
+    computer = 0
+    console.log("Lets play Rock,Paper or Scissors")
+    for (let i = 0; i < 5; i++) {
+        // your code here!
+        if (playRound == "You win"){
+            player += 1
+        } else if (playRound = 'Play again!'){
+            continue
+        } else{
+            computer += 1
+        }
+    if (player > computer){
+        console.log("Congratulations You win with " + player)
+    }else{
+        console.log('You lose with '+ computer)
+    }
+
+     }
+
+  }
+
 
