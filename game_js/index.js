36ca2d8577933990fad0db365e10f33386b3fe9d
Jetro Oropeza <jetrojackof@hotmail.com>
2022-06-04 21:11:32 -0500

    fix loop for 5 games complete the counter

--- game_js/index.js
+++ game_js/index.js
@@ -3,12 +3,11 @@ function computerPlay(){
     return arr[Math.floor(Math.random()*arr.length)]
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+//const playerSelection = "rock";
+//const computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
 
 function playRound( playerSelection, computerSelection) {
-    playerSelection.toLowerCase()
     console.log(computerSelection)
     if (playerSelection  === computerSelection) {
         return "Play again!"
@@ -23,55 +22,42 @@ function playRound( playerSelection, computerSelection) {
     }
 
   }
-    //*Play Round test *//
-
-    //const playerSelection = "rock";
-    //const computerSelection = computerPlay();
-    //console.log(playRound(playerSelection, computerSelection))
-
-
-function game(playRound){
-    player = 0
-    computer = 0
-    console.log("Lets play Rock,Paper or Scissors")
-    for (let i = 0; i < 5; i++) {
-        // your code here!
-        if (playRound == "You win"){
-            player += 1
-        } else if (playRound = 'Play again!'){
-            continue
-        } else{
-            computer += 1
-        }
-    if (player > computer){
-        console.log("Congratulations You win with " + player)
-    }else{
-        console.log('You lose with '+ computer)
-    }
-
-     }
-
+  
+  function playerPlay(){
+      let playerChoice = prompt("Choose Rock Paper or Scissors").toLowerCase()
+      return playerChoice
   }
   
   function game() {
     //Play game 5 times
     player =0
     computer =0
-    round = 0
+
     for (let i = 0; i < 5; i++) {
-      const playerSelection = playerPlay();
-      const computerSelection = computerPlay();
-      const currentRound = playRound(playerSelection, computerSelection);
-      //result
-      console.log(currentRound);
+        const playerSelection = playerPlay();
+        const computerSelection = computerPlay();
+        const currentRound = playRound(playerSelection, computerSelection);
+        //result
+        console.log(currentRound);
+        if (currentRound == "You win"){
+            player += 1
+        } else if (currentRound == "Play again!"){
+            continue
+        } else {
+            computer += 1
+        }
+    console.log("Player score " + player)
+    console.log("Computer score "+ computer)
+
+    
     }
-    if (currentRound == "You win"){
-        player += 1
-    } else if (currentRound = 'Play again!'){
-        round +=1
-    } else {
-        computer += 1
-    }
+    //if (currentRound == "You win"){
+    //    player += 1
+    //} else if (currentRound = 'Play again!'){
+    //    round +=1
+    //} else {
+    //    computer += 1
+    //}
  }
 
 
